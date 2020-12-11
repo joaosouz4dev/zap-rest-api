a6dba853d36342bb307dedc33e4bd66a84e10b56
João Victor Souza <souza.joao1997@gmail.com>
2020-12-11 10:27:04 -0300

    feat: a best way to read qrcode

--- index.js
+++ index.js
@@ -1,150 +1,147 @@
-const express = require('express')
+const express = require('express');
 const venom = require('venom-bot');
 const fs = require('fs');
+const path = require('path');
 const router = express();
 
-const port = 3000;
-var clientVenom;
+router.set('view engine', 'pug');
+router.set('views', path.join(__dirname, 'views'));
 
+const port = 3000;
+var clientVenom,
+	clientQrcode,
+	clientvalidator = !1;
 
 // iniciando venom
 router.get('/init', (req, res) => {
-    if (!clientVenom || clientVenom == false) {
-        res.status(200).json({
-            success: true,
-            message: 'Venom foi iniciado, leia o qrcode'
-        });
-        if (fs.existsSync(__dirname +'/qrcode.png')) {
-            fs.unlinkSync(__dirname +'/qrcode.png');
-        }
-        venom
-            .create(
-                // session
-                'clientVenom',
-                // exporting qrcode
-                (base64Qr) => {
-                    var matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
-                        response = {};
-
-                    if (matches.length !== 3) {
-                        return new Error('Invalid input string');
-                    }
-                    response.type = matches[1];
-                    response.data = new Buffer.from(matches[2], 'base64');
-
-                    var imageBuffer = response;
-                    fs.writeFile(
-                        'qrcode.png',
-                        imageBuffer['data'],
-                        'binary',
-                        function (err) {
-                            if (err != null) {
-                                console.log(err);
-                            }
-                            console.log('qrcode exportado');
-                        }
-                    );
-
-                },
-                // statusFind
-                (statusSession) => {
-                    
-                    console.log('Status Session: ', statusSession);
-                },
-                // options
-                {
-                    headless: true,
-                    devtools: false,
-                    useChrome: true,
-                    debug: false,
-                    logQR: true,
-                    browserWS: '',
-                    disableSpins: true,
-                    disableWelcome: true,
-                    updatesLog: true,
-                    autoClose: 60000
-                }
-            )
-            .then((client) => {
-                if (fs.existsSync(__dirname +'/qrcode.png')) {
-                    fs.unlinkSync(__dirname +'/qrcode.png');
-                }
-                clientVenom = client;
-            })
-            .catch((erro) => {
-                clientVenom = false;
-                console.log(erro);
-            });
-    } else {
-        res.status(200).json({
-            success: true,
-            message: 'venom já foi lido'
-        });
-    }
+	if (!clientvalidator) {
+		clientvalidator = !0;
+		res.status(200).json({
+			success: !0,
+			message: 'Venom foi iniciado, leia o qrcode'
+		});
+		try {
+			venom
+				.create(
+					// session
+					'clientVenom',
+					// exporting qrcode
+					(base64Qr) => {
+						clientQrcode = base64Qr;
+					},
+					// statusFind
+					(statusSession) => {
+						const conflits = [
+							'browserClose',
+							'qrReadFail',
+							'autocloseCalled',
+							'serverClose',
+							'deleteToken'
+						];
+						if (conflits.includes(statusSession)) {
+							clientvalidator = !1;
+							clientVenom = !1;
+						}
+						console.log('Status Session: ', statusSession);
+					},
+					// options
+					{
+						headless: !0,
+						devtools: !1,
+						useChrome: !0,
+						debug: !1,
+						logQR: !0,
+						disableSpins: !0,
+						disableWelcome: !0,
+						updatesLog: !0,
+						autoClose: 60000
+					}
+				)
+				.then((client) => {
+					if (clientQrcode) {
+						clientQrcode = !1;
+					}
+					clientVenom = client;
+				})
+				.catch((erro) => {
+					clientvalidator = !1;
+					clientVenom = !1;
+					console.log(erro);
+				});
+		} catch (error) {
+			clientvalidator = !1;
+			clientVenom = !1;
+		}
+	} else {
+		res.status(200).json({
+			success: !0,
+			message: 'venom já foi lido'
+		});
+	}
 });
 
 // deletando cliente
 router.get('/deleteCliente', (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: 'Cliente deletado'
-    });
+	res.status(200).json({
+		success: !0,
+		message: 'Cliente deletado'
+	});
 });
 
 // deletando cliente
 router.get('/qrcode', (req, res) => {
-    if (fs.existsSync(__dirname +'/qrcode.png')) {
-        res.sendFile('qrcode.png', { root: __dirname });
-    }else{
-        if (clientVenom && clientVenom != false) {
-            res.status(200).json({
-                success: true,
-                message: 'Venom já foi lido'
-            });
-        }else{
-            res.status(400).json({
-                success: false,
-                message: 'Qrcode ainda não foi exportado'
-            });
-        }
-    }
+	if (clientQrcode) {
+		res.render('qrcode', { qr: clientQrcode });
+	} else {
+		if (clientvalidator) {
+			res.status(200).json({
+				success: !0,
+				message: 'Venom já foi lido'
+			});
+		} else {
+			res.status(400).json({
+				success: !1,
+				message: 'Qrcode ainda não foi exportado'
+			});
+		}
+	}
 });
 
 // Enviar mensagens
 router.get('/enviarmsg', async (req, res) => {
-    let chatId = req.query.numero;
-    let msg = req.query.msg;
-    if (clientVenom && clientVenom != false) {
-        if (chatId && msg) {
-            msg = msg.replace(/<br>/gi, "\n");
-            try {
-                let resp = await clientVenom.sendText(chatId + '@c.us', '' + msg);
-                res.status(200).json({
-                    success: true,
-                    message: resp
-                });
-            } catch (e) {
-                console.log(e);
-                res.status(500).json({
-                    success: false,
-                    message: 'Algo deu errado!'
-                });
-            }
-        } else {
-            res.status(400).json({
-                success: false,
-                message: 'Request imcompleto, necessita revisão!'
-            });
-        }
-    } else {
-        res.status(400).json({
-            success: false,
-            message: 'Venom ainda não foi lido corretamente!'
-        });
-    }
+	let chatId = req.query.numero;
+	let msg = req.query.msg;
+	if (clientvalidator) {
+		if (chatId && msg) {
+			msg = msg.replace(/<br>/gi, '\n');
+			try {
+				let resp = await clientVenom.sendText(chatId + '@c.us', '' + msg);
+				res.status(200).json({
+					success: !0,
+					message: resp
+				});
+			} catch (e) {
+				console.log(e);
+				res.status(500).json({
+					success: !1,
+					message: 'Algo deu errado!'
+				});
+			}
+		} else {
+			res.status(400).json({
+				success: !1,
+				message: 'Request imcompleto, necessita revisão!'
+			});
+		}
+	} else {
+		res.status(400).json({
+			success: !1,
+			message: 'Venom ainda não foi lido corretamente!'
+		});
+	}
 });
 
 // iniciando express na porta setada.
 router.listen(port, () => {
-    console.log(`Api inicada na porta: ${port}`)
+	console.log(`Api started at the port: ${port}`);
 });
